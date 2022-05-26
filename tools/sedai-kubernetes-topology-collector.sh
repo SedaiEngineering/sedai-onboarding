fac4d9619966c33383407cecae4762721babb059
dijeesh <dijeeshpnair@gmail.com>
2022-05-26 19:19:08 +0530

    fix collector script

--- tools/sedai-kubernetes-topology-collector.sh
+++ tools/sedai-kubernetes-topology-collector.sh
@@ -12,22 +12,26 @@ LABEL="sedai-kube-topology-reader"
 red="\033[1;31m"
 green="\033[1;32m"
 
+# Fectch Deployment Configuration.
 curl https://raw.githubusercontent.com/SedaiEngineering/sedai-onboarding/main/tools/sedai-kubernetes-topology-collector.yaml -o sedai-kubernetes-topology-collector.yaml
 
+# Run collector Job.
 echo -e "\e[1;32mLaunching Collector!"$rescolor""
 kubectl apply -f sedai-kubernetes-topology-collector.yaml
-sleep 90
+sleep 30
 
-toppod=$(kubectl get pods --selector="app=$LABEL" | grep "$LABEL" | awk '{print $3}')
-    if [[ ! "$toppod" =~ ^Running$|^Completed$  ]]  ; then
+# Verify and pull topology.json file.
+topologypod=$(kubectl get pods --selector="app=$LABEL" | grep "$LABEL" | awk '{print $1}')
+status=$(kubectl get pods --selector="app=$LABEL" | grep "$LABEL" | awk '{print $3}')
+    if [[ ! "$status" =~ ^Running$|^Completed$  ]]  ; then
         echo -e "\e[1;31mError !"$rescolor""
         exit
     else
         echo -e "\e[1;32mOK!"$rescolor""
-        kubectl cp "$toppod":/home/sedai/topology.json topology.json 
+        kubectl cp "$topologypod":/home/sedai/topology.json topology.json 
         echo " "
         echo "Starting Cleanup"
         kubectl delete -f sedai-kubernetes-topology-collector.yaml
         echo "Cleanup Complete"
-        echo "Please send the topology.json Sedai Team"
+        echo "Please send the topology.json to Sedai Team"
     fi
